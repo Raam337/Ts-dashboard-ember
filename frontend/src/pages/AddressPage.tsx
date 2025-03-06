c9177208e485f01ca38be07a6793158f5136d7e5
RobF <raam337@gmail.com>
2025-03-06 15:38:51 +0200

    Responsive design 2

--- frontend/src/pages/AddressPage.tsx
+++ frontend/src/pages/AddressPage.tsx
@@ -3,14 +3,14 @@ import { AddressDetails, AddressList } from "@/components/components";
 export default function AddressPage() {
   return (
     <main className="
-      grid  grid-cols-1 h-[calc(100vh-60px)]
-      md:grid-cols-[300px_1fr] md:h-[calc(100vh-60px)]
+      grid grid-cols-1 grid-rows-[5fr_1fr] h-[calc(100vh-60px)]
+      md:grid-cols-[300px_1fr] md:grid-rows-1 md:h-[calc(100vh-60px)]
     ">
-      <aside className="bg-gray-200 overflow-auto h-full md:h-full">
+      <aside className="bg-gray-200 overflow-auto h-full">
         <AddressList />
       </aside>
 
-      <section className="p-2 min-h-[60px]">
+      <section className="p-2">
         <AddressDetails></AddressDetails>
       </section>
     </main>
